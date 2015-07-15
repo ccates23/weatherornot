afd7d9aed5e3f69f78f77a416343bf2ae7bb9e77
chadcates <cates.chad@gmail.com>
2015-07-15 10:41:29 -0500

    created settings.html and settings.js with controllers

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.search', 'starter.weather'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.search', 'starter.weather', 'starter.settings'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -39,7 +39,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.search', 'st
       }
     }
   })
-
+    .state('app.settings', {
+    url: '/settings',
+    views: {
+      'menuContent': {
+        templateUrl: "templates/settings/settings.html",
+        controller: 'SettingsCtrl'
+      }
+    }
+  })
   .state('app.weather', {
     url: "/weather/:city/:lat/:long",
     views: {
